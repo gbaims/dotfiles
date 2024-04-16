a0d6b49adf4f99985bbadb6643f83be5df306dc2
Gabriel Baims <gbaims@users.noreply.github.com>
2024-04-16 18:44:49 -0300

    add neovim from unstable repo

--- .shell/debian-install.bash
+++ .shell/debian-install.bash
@@ -2,7 +2,9 @@
 
 _dir="`pwd`"
 _packages=(
+    build-essential
     git
+    ripgrep
 
     xorg
     xserver-xorg-video-all
@@ -14,6 +16,9 @@ _packages=(
 
     kitty
 )
+_unstable_packages=(
+    neovim
+)
 
 #
 _echo() {
@@ -23,8 +28,27 @@ _echo() {
     fi
 }
 
+config_repositories() {
+    _echo "[*] Adding 'testing' and 'unstable' apt sources"
+    sudo tee "/etc/apt/sources.list.d/20-newer-packages.list" > /dev/null <<- EOF
+		deb http://deb.debian.org/debian testing main contrib
+		deb http://deb.debian.org/debian unstable main contrib
+	EOF
+
+    sudo tee "/etc/apt/preferences.d/20-newer-packages" > /dev/null <<- EOF
+		Package: *
+		Pin: release a=testing
+		Pin-Priority: -10
+
+		Package: *
+		Pin: release a=unstable
+		Pin-Priority: -20
+	EOF
+}
+
 ## Perform system upgrade
 upgrade_system() {
+    _echo "[*] Performing system upgrade"
     sudo apt-get update --yes
     if [[ "$?" != '0' ]]; then
         _echo "[!] Failed to retrieve new lists of packages" 1  
@@ -39,17 +63,38 @@ install_pkgs() {
     _echo "[*] Installing required packages..."
     for _pkg in "${_packages[@]}"; do
         _echo "[+] Installing package : $_pkg"
-	sudo apt-get install "$_pkg" --yes
-	if [[ "$?" != '0' ]]; then
+        sudo apt-get install "$_pkg" --yes
+        if [[ "$?" != '0' ]]; then
             _echo "[!] Failed to install package: $_pkg"
             _failed_to_install+=("$_pkg")
-	fi
+        fi
     done
 
     # List failed packages
     echo
     for _failed in "${_failed_to_install[@]}"; do
         _echo "[!] Failed to install package: ${_failed}"
+    done
+    if [[ -n "${_failed_to_install}" ]]; then
+        _echo "[!] Install these packages manually to continue, exiting..." 1
+    fi
+}
+
+install_unstable_pkgs() {
+    _echo "[*] Installing required unstable packages..."
+    for _pkg in "${_unstable_packages[@]}"; do
+        _echo "[+] Installing package : $_pkg"
+        sudo apt-get install "$_pkg"/unstable --yes
+        if [[ "$?" != '0' ]]; then
+            _echo "[!] Failed to install package: $_pkg"
+            _failed_to_install+=("$_pkg")
+        fi
+    done
+
+    # List failed packages
+    echo
+    for _failed in "${_failed_to_install[@]}"; do
+        _echo "[!] Failed to install unstable package: ${_failed}"
     done
     if [[ -n "${_failed_to_install}" ]]; then
         _echo "[!] Install these packages manually to continue, exiting..." 1
@@ -64,6 +109,8 @@ install_starship() {
     fi
 }
 
+config_repositories
 upgrade_system
 install_pkgs
+install_unstable_pkgs
 install_starship
